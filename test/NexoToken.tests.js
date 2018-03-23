c4f60af724071a7d14aa0960035e79f102c82595
Valdar13 <Valdar13@users.noreply.github.com>
2018-03-23 12:43:07 +0300

    add test file

---
+++ test/NexoToken.tests.js
@@ -0,0 +1,18 @@
+import assertRevert from './helpers/assertRevert';
+
+var token = artifacts.require('../contracts/NexoToken.sol');
+const increaseTime = require('./utils/time_travel');
+const BigNumber = web3.BigNumber;
+
+require('chai')
+  .use(require('chai-as-promised'))
+  .use(require('chai-bignumber')(BigNumber))
+  .should();
+
+
+contract('NexoTokens Contract', function(accounts) {
+
+
+
+
+});
