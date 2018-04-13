e383420aee3d7ab0c60c757e837167ca3063ac01
Alex Bash <avbashlykov@gmail.com>
2018-04-13 18:52:43 +0300

    override confirmOwnership() to make sure that the old owner can not withdraw tokens from reserves

--- contracts/NexoToken.sol
+++ contracts/NexoToken.sol
@@ -260,6 +260,26 @@ contract NexoToken is Token {
 		return sub(unlockedTokens, spentTokens);
 	}
 
+	/// @dev Overrides Owned.sol function
+    function confirmOwnership()
+        public
+        onlyPotentialOwner
+    {   
+        // Forbid the old owner to distribute investors' tokens
+        allowed[investorsAllocation][owner] = 0;
+
+        // Allow the new owner to distribute investors' tokens
+        allowed[investorsAllocation][msg.sender] = balanceOf(investorsAllocation);
+
+        // Forbid the old owner to withdraw any tokens from the reserves
+        allowed[overdraftAllocation][owner] = 0;
+        allowed[teamAllocation][owner] = 0;
+        allowed[communityAllocation][owner] = 0;
+        allowed[advisersAllocation][owner] = 0;
+
+        super.confirmOwnership();
+    }
+
 	function _calculateUnlockedTokens(
 		uint256 _cliff,
 		uint256 _periodLength,
