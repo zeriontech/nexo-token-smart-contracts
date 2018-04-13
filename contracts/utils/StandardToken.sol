cfaa64885dafdf4452d21ee639af1a0e4a74a520
Alex Bash <avbashlykov@gmail.com>
2018-04-13 17:17:51 +0300

    formatting

--- contracts/utils/StandardToken.sol
+++ contracts/utils/StandardToken.sol
@@ -77,13 +77,9 @@ contract StandardToken is AbstractToken, Owned, SafeMath {
 	function _transfer(address _from, address _to, uint256 _value) private returns (bool success) {
 		require(_to != address(0));
 		require(balances[_from] >= _value);
-
 		balances[_from] -= _value;
-
 		balances[_to] = add(balances[_to], _value);
-
 		emit Transfer(_from, _to, _value);
-
 		return true;
 	}
 }
