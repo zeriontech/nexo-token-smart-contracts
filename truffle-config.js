5b6de2d5cacc46e41b65afaa1852db8f28cd841e
Alex Bash <avbashlykov@gmail.com>
2018-04-13 16:36:07 +0300

    cevian's fixes

--- truffle-config.js
+++ truffle-config.js
@@ -3,18 +3,18 @@ require('babel-polyfill');
 
 
 module.exports = {
-  networks: {
-    development: {
-      host: '127.0.0.1',
-      port: 8545,
-      network_id: '*', // eslint-disable-line camelcase
-    },
-    coverage: {
-      host: 'localhost',
-      network_id: '*', // eslint-disable-line camelcase
-      port: 8555,
-      gas: 0xfffffffffff,
-      gasPrice: 0x01,
-    },
-  },
+	networks: {
+		development: {
+			host: '127.0.0.1',
+			port: 8545,
+			network_id: '*', // eslint-disable-line camelcase
+		},
+		coverage: {
+			host: 'localhost',
+			network_id: '*', // eslint-disable-line camelcase
+			port: 8555,
+			gas: 0xfffffffffff,
+			gasPrice: 0x01,
+		},
+	},
 };
